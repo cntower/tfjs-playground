4e8ff5c937db5e7c60423a21ba97e192afb33664
Stanislav Baturin <stasbaturin78@gmail.com>
2019-11-05 08:08:55 +0300

    debug

--- src/app/linear/linear.component.ts
+++ src/app/linear/linear.component.ts
@@ -1,12 +1,12 @@
 import * as tf from '@tensorflow/tfjs';
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-linear',
   templateUrl: './linear.component.html',
   styleUrls: ['./linear.component.css']
 })
-export class LinearComponent implements OnInit, AfterViewInit {
+export class LinearComponent implements AfterViewInit {
 
   @ViewChild('canvas', { static: true }) canvas !: ElementRef<HTMLCanvasElement>;
   ctx: CanvasRenderingContext2D;
@@ -25,7 +25,7 @@ export class LinearComponent implements OnInit, AfterViewInit {
     this.height = +this.canvas.nativeElement.getAttribute('height');
     this.width = +this.canvas.nativeElement.getAttribute('width');
     this.ctx.fillStyle = 'rgba(1,1,1,1)';
-    this.ctx.fillRect(0, 0, this.height, this.width);
+    this.ctx.fillRect(0, 0, this.width, this.height);
 
     this.m = tf.variable(tf.scalar(Math.random()));
     this.b = tf.variable(tf.scalar(Math.random()));
@@ -38,42 +38,48 @@ export class LinearComponent implements OnInit, AfterViewInit {
     const mousePos = getMousePos(this.canvas.nativeElement, event);
     this.scaledPoints.push({
       y: scale(mousePos.y, 0, this.height, 1, 0),
-      x: scale(mousePos.x, 0, this.height, 0, 1)
+      x: scale(mousePos.x, 0, this.width, 0, 1)
     });
     this.draw();
 
   }
 
-  ngOnInit() {
-
-
-  }
-
   draw() {
-    this.ctx.fillRect(0, 0, this.height, this.width);
-    this.ctx.strokeStyle = 'rgba(200,200,1,1)';
-    this.scaledPoints.forEach((point) => {
-      this.drawPoint(point.x, point.y, 5);
-    });
+
     if (this.scaledPoints.length > 0) {
       tf.tidy(() => {
-        const ys = tf.tensor1d(this.scaledPoints.map(p => p.y));
-        this.optimizer.minimize(() => this.loss(this.predict(this.scaledPoints.map(p => p.x)), ys));
-        const xs = [0, 1];
-        const lineYs = this.predict(xs);
-        const x1 = scale(xs[0], 0, 1, 0, this.width);
-        const x2 = scale(xs[1], 0, 1, 0, this.width);
-        lineYs.data().then(data => {
-          const y1 = scale(data[0], 0, 1, this.width, 0);
-          const y2 = scale(data[1], 0, 1, this.width, 0);
-          this.ctx.moveTo(x1, y1);
-          this.ctx.lineTo(x2, y2);
+        this.trainingStep().then((line) => {
+          this.ctx.fillRect(0, 0, this.width, this.height);
+          this.ctx.strokeStyle = 'rgba(200,200,1,1)';
+          this.scaledPoints.forEach((point) => {
+            this.drawPoint(point.x, point.y, 8);
+          });
+          this.ctx.lineWidth = 3;
+          this.ctx.moveTo(line.x1, line.y1);
+          this.ctx.lineTo(line.x2, line.y2);
           this.ctx.stroke();
-          console.log(this.m.toString(), tf.memory().numTensors);
         });
       });
 
     }
+  }
+
+  trainingStep() {
+
+    const ys = tf.tensor1d(this.scaledPoints.map(p => p.y));
+    this.optimizer.minimize(() => this.loss(this.predict(this.scaledPoints.map(p => p.x)), ys));
+    const xs = [0, 1];
+    const lineYs = this.predict(xs);
+    const x1 = scale(xs[0], 0, 1, 0, this.width);
+    const x2 = scale(xs[1], 0, 1, 0, this.width);
+    return lineYs.data().then(data => {
+      const y1 = scale(data[0], 0, 1, this.height, 0);
+      const y2 = scale(data[1], 0, 1, this.height, 0);
+
+      // console.log(this.m.toString(), tf.memory().numTensors);
+      return { x1, x2, y1, y2 };
+    });
+
   }
 
   drawPoint(x, y, lineWidth: number = this.ctx.lineWidth) {
