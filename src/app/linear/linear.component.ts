aded85188ab7ede5efb1401c8da61a2ae8a855d7
Stanislav Baturin <stasbaturin78@gmail.com>
2019-12-08 22:50:39 +0200

    change inner solve space from [0:1] to [-1:1]

--- src/app/linear/linear.component.ts
+++ src/app/linear/linear.component.ts
@@ -1,6 +1,6 @@
 import * as tf from '@tensorflow/tfjs';
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-
+interface Point2d { x: number; y: number; }
 @Component({
   selector: 'app-linear',
   templateUrl: './linear.component.html',
@@ -12,12 +12,13 @@ export class LinearComponent implements AfterViewInit {
   ctx: CanvasRenderingContext2D;
   height: number;
   width: number;
-  scaledPoints: { x: number, y: number }[] = [];
+  scaledPoints: Point2d[] = [];
   m: tf.Variable<tf.Rank.R0>;
   b: tf.Variable<tf.Rank.R0>;
   learningRate = 0.1;
   optimizer = tf.train.sgd(this.learningRate);
   loss = (pred, label) => pred.sub(label).square().mean();
+  predict = (xs) => tf.tensor1d(xs).mul(this.m).add(this.b);
 
   constructor() { }
   ngAfterViewInit(): void {
@@ -36,10 +37,13 @@ export class LinearComponent implements AfterViewInit {
 
   onCanvasClick(event) {
     const mousePos = getMousePos(this.canvas.nativeElement, event);
-    this.scaledPoints.push({
-      y: scale(mousePos.y, 0, this.height, 1, 0),
-      x: scale(mousePos.x, 0, this.width, 0, 1)
-    });
+    const scaledPoint: Point2d = {
+      y: scale(mousePos.y, 0, this.height, 1, -1),
+      x: scale(mousePos.x, 0, this.width, -1, 1)
+    };
+    this.scaledPoints.push(scaledPoint);
+    const naturX = scale(scaledPoint.x, -1, 1, 0, this.width);
+    const naturY = scale(scaledPoint.y, -1, 1, this.height, 0);
     this.draw();
 
   }
@@ -68,13 +72,13 @@ export class LinearComponent implements AfterViewInit {
 
     const ys = tf.tensor1d(this.scaledPoints.map(p => p.y));
     this.optimizer.minimize(() => this.loss(this.predict(this.scaledPoints.map(p => p.x)), ys));
-    const xs = [0, 1];
+    const xs = [-1, 1];
     const lineYs = this.predict(xs);
-    const x1 = scale(xs[0], 0, 1, 0, this.width);
-    const x2 = scale(xs[1], 0, 1, 0, this.width);
+    const x1 = scale(xs[0], -1, 1, 0, this.width);
+    const x2 = scale(xs[1], -1, 1, 0, this.width);
     return lineYs.data().then(data => {
-      const y1 = scale(data[0], 0, 1, this.height, 0);
-      const y2 = scale(data[1], 0, 1, this.height, 0);
+      const y1 = scale(data[0], -1, 1, this.height, 0);
+      const y2 = scale(data[1], -1, 1, this.height, 0);
 
       // console.log(this.m.toString(), tf.memory().numTensors);
       return { x1, x2, y1, y2 };
@@ -83,8 +87,8 @@ export class LinearComponent implements AfterViewInit {
   }
 
   drawPoint(x, y, lineWidth: number = this.ctx.lineWidth) {
-    const naturX = scale(x, 0, 1, 0, this.width);
-    const naturY = scale(y, 0, 1, this.height, 0);
+    const naturX = scale(x, -1, 1, 0, this.width);
+    const naturY = scale(y, -1, 1, this.height, 0);
     this.ctx.lineWidth = lineWidth;
     this.ctx.beginPath();
     this.ctx.lineCap = 'round';
@@ -93,15 +97,8 @@ export class LinearComponent implements AfterViewInit {
     this.ctx.stroke();
   }
 
-  predict(xs) {
-    const tfys = tf.tensor1d(xs).mul(this.m).add(this.b);
-    return tfys;
-  }
-
 }
 
-// Get Mouse Position
-// tslint:disable-next-line: no-shadowed-variable
 function getMousePos(canvas: HTMLCanvasElement, evt: MouseEvent) {
   const rect = canvas.getBoundingClientRect();
   return {
@@ -112,5 +109,5 @@ function getMousePos(canvas: HTMLCanvasElement, evt: MouseEvent) {
 
 function scale(x: number, xMin: number, xMax: number, scaledXMin: number, scaledXMax: number) {
   const scaleFakrtor = (scaledXMax - scaledXMin) / (xMax - xMin);
-  return scaleFakrtor * x + scaledXMin;
+  return scaleFakrtor * (x - xMin) + scaledXMin;
 }
